61061d0b45f1a90545781bccfc01de0b1b10c536
Ayesha Najeeb <130223342+Ayesha-328@users.noreply.github.com>
2024-08-19 09:44:26 +0500

    Update socket.js final

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "https://threads-clone-mern-client.vercel.app",
+		origin: "https://threads-app-clone-six.vercel.app",
 		methods: ["GET", "POST"],
 		credentials: true
 	},
