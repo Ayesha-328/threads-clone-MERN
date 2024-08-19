d1a9a4ab16d6e73b114ff03b65ba47c488402bc9
Ayesha Najeeb <130223342+Ayesha-328@users.noreply.github.com>
2024-08-19 08:25:33 +0500

    Update server.js x

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ cloudinary.config({
 
 // Middlewares => The funcs that run between req and res
 app.use(cors({
-  origin: 'https://threads-clone-mern-frontend-cfsyghr0c-ayesha-328s-projects.vercel.app',
+  origin: 'http://localhost:3000',
   credentials: true,
 }));
 
